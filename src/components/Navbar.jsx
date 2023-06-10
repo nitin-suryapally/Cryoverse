088d270abc9613682bdc67483af345b2e538a079
Nitin Suryapally <nitin28suryapally@gmail.com>
2023-06-10 19:15:27 +0530

    name changed

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -35,7 +35,7 @@ const Navbar = () => {
             <div className="logo-container">
                 <Avatar src={icon} size="large" />
                 <Typography.Title level={2} className="logo">
-                    <Link to="/">Cryptoverse</Link>
+                    <Link to="/">CryptoPedia</Link>
                 </Typography.Title>
                 <Button className="menu-control-container" onClick={() => setActiveMenu(!activeMenu)}><MenuOutlined /></Button>
             </div>
